41151a88cac7643c1472f2e62b00bd208c25b919
utkarsh1025 <utkarshmaurya.707@gmail.com>
2021-05-08 21:27:15 +0530

    first push

---
+++ js/main.js
@@ -0,0 +1,7 @@
+ // typing text animation script
+ var typed = new Typed(".typing", {
+    strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
